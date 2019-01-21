c2478437c01e186d2841936d2df805b85cebe587
Vladius25 <vkorol2509@icloud.com>
2019-01-21 07:15:35 +0500

    Full messager update

--- src/messager.py
+++ src/messager.py
@@ -1,53 +1,43 @@
-#!usr/bin/python3.6
-import select
-import socket
-import sys
-import threading
+#!/usr/bin/python3
+"""Script for chat client."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
 from coder import Encoder, Decoder
+from time import sleep
 import configparser
+import sys
 
-def promt():
-    sys.stdout.write("<YOU>:")
-    sys.stdout.flush()
 
-def recv_message(socket):
+def receive():
+    """Handles receiving of messages."""
     while True:
-        read_socket, write_socket, error_socket = select.select([socket], [], [])
-
-        if len(read_socket) >= 1:
-            try:
-                message = read_socket[0].recv(1024).decode("utf-8")
-                sys.stdout.write(message)
-            except Exception as e:
-                print("Can't received ", e)
-
-
-def send_message(socket):
-    while True:
-        message = sys.stdin.readline()
         try:
-            socket.send(encoder.encode(message))
-            promt()
-        except Exception as e:
-            sys.stdout.write(e)
-            sys.exit(1)
+            msg = client_socket.recv(BUFSIZ)
+            sys.stdout.write(decoder.decode(msg))
+        except OSError:  # Possibly client has left the chat.
+            break
 
 
-if __name__ == "__main__":
-    config = configparser.ConfigParser()
-    config.read('configs/config.ini')
-    HOST, PORT = config['DEFAULT']['SERVER_IP'], config['DEFAULT']['MES_PORT']
+def send():
+    """Handles sending of messages."""
+    while True:
+        msg = sys.stdin.readline()
+        client_socket.send(encoder.encode(msg))
 
-    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.settimeout(2)
-    encoder, decoder = Encoder(), Decoder()
 
-    try:
-        socket.connect((HOST, int(PORT)))
-    except Exception as e:
-        print("Connection failed: ", e)
-        sys.exit(1)
-    
-    promt()
-    threading.Thread(target=send_message, args=(socket,)).start()
-    threading.Thread(target=recv_message, args=(socket,)).start()
+encoder, decoder = Encoder(), Decoder()
+config = configparser.ConfigParser()
+config.read('configs/config.ini')
+
+# ----Now comes the sockets part----
+HOST, PORT = config['DEFAULT']['SERVER_IP'], config['DEFAULT']['KEY_PORT']
+
+BUFSIZ = 6144
+ADDR = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+Thread(target=receive).start()
+sleep(1)
+Thread(target=send).start()
