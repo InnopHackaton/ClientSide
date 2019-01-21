7eee17f3daff10b7ee8f327a2d1b8678339447cf
Vladius25 <vkorol2509@icloud.com>
2019-01-21 07:16:10 +0500

    Reverse cipher

--- src/coder.py
+++ src/coder.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3.6
 import gnupg
+from Crypto.Cipher import AES
+import base64
+import configparser
+
 
 class Encoder():
     def __init__(self):
@@ -7,8 +11,18 @@ class Encoder():
 
     def encode(self, message):
         data = str(self.gpg.encrypt(message, "hack@hack.ru", always_trust=True, armor=True))
-        result = data.replace("\n", "").replace("-----BEGIN PGP MESSAGE-----", "").replace("-----END PGP MESSAGE-----", "")
+        result = data.replace("\n", "").replace("-----BEGIN PGP MESSAGE-----", "").replace("-----END PGP MESSAGE-----",
+                                                                                           "")
         return bytes(result, "utf-8")
 
+
 class Decoder():
-        pass
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('configs/config.ini')
+
+        self.secret_key = config["DEFAULT"]["SECRET_KEY"]
+        self.cipher = AES.new(self.secret_key, AES.MODE_ECB)
+
+    def decode(self, message):
+        return self.cipher.decrypt(base64.b64decode(message)).decode("utf-8").lstrip()
